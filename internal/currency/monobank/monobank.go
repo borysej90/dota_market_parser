dad56f8094fd9b1f4fe699f490eff32fbdf736b5
borysej90 <borysej90@gmail.com>
2021-09-28 13:16:02 +0300

    Created Monobank currency converted

---
+++ internal/currency/monobank/monobank.go
@@ -0,0 +1,61 @@
+package monobank
+
+import (
+	"dota_market_notifier/internal/currency"
+	"encoding/json"
+	"github.com/pkg/errors"
+	"net/http"
+	"time"
+)
+
+var _ currency.Parser = &MonoParser{}
+
+const MonoURL = "https://api.monobank.ua/bank/currency"
+
+type currencyData struct {
+	CodeA    int     `json:"currencyCodeA,omitempty"`
+	CodeB    int     `json:"currencyCodeB,omitempty"`
+	RateBuy  float32 `json:"rateBuy,omitempty"`
+	RateSell float32 `json:"rateSell,omitempty"`
+}
+
+type MonoParser struct {
+	rates    map[int]float32
+	validTil time.Time
+}
+
+func (m *MonoParser) GetCurrencyRate(currencyName string) (float32, error) {
+	if m.rates == nil || m.validTil.Before(time.Now()) {
+		if err := m.fetchAPI(); err != nil {
+			return 0, err
+		}
+	}
+	currencyCode, ok := currency.Currency[currencyName]
+	if !ok {
+		return 0, errors.Errorf("unsupported currency: %s", currencyName)
+	}
+	return m.rates[currencyCode], nil
+}
+
+func (m *MonoParser) fetchAPI() error {
+	client := http.Client{Timeout: 30 * time.Second}
+	resp, err := client.Get(MonoURL)
+	if err != nil {
+		return err
+	}
+	var res []currencyData
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return err
+	}
+	for _, curr := range res {
+		if curr.CodeB != currency.Currency["UAH"] {
+			continue
+		}
+		switch curr.CodeA {
+		case currency.Currency["USD"], currency.Currency["EUR"], currency.Currency["RUB"]:
+			m.rates[curr.CodeA] = curr.RateSell
+		}
+	}
+	m.validTil = time.Now().Add(5 * time.Minute)
+	return nil
+}
